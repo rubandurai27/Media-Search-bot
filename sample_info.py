82e6cbd1c3f093ec51dd0f524b2afab004aa1b85
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2021-09-11 20:20:00 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -23,9 +23,9 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I Am Auto Filter bot**
 
-Here you can search files in inline mode. Just press follwing buttons and start searching.
+Here you can search files in inline mode. Just press follwing buttons and start searching. @TNROCKERS2021
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
